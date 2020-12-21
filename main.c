b3273ab16f5e7907b013093a6a4a213c54dfa152
Jhon Arias <7honarias@gmail.com>
2020-12-21 15:46:33 +0000

    [UPDATE] - error when file Can't open

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	p_file = fopen(argv[1], "r");
 	if (p_file == NULL)
 	{
-		fprintf(stderr, "Error: Can't open file <%s>", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while ((read = getline(&line, &len, p_file)) != -1)

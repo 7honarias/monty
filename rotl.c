c08bec697bc21fbc164772f16703da679b4368bb
Jhon Arias <7honarias@gmail.com>
2020-12-22 12:03:05 +0000

    [UPDATE] - Correction in var

--- rotl.c
+++ rotl.c
@@ -7,23 +7,24 @@
  * Return: Nothing
  */
 
-void op_rotl(stack_t **stack, __attribute__((unused))unsigned int line_number)
+void op_rotl(stack_t **stack, __attribute__((unused)) unsigned int line_number)
 {
 	stack_t *temp;
-	stack_t *run = *stack;
+	stack_t *run;
 
-	if (stack == NULL || *stack == NULL)
-		return;
-
-	temp = *stack;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-
-	while (run->next)
+	if (*stack)
 	{
-		run = run->next;
+		temp = *stack;
+		run = *stack;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+
+		while (run->next)
+		{
+			run = run->next;
+		}
+		run->next = temp;
+		temp->prev = run;
+		temp->next = NULL;
 	}
-	temp->prev = run;
-	temp->next = NULL;
-	run->next = temp;
 }

834594b1a64f418de214ee0211fc7bb144fce4e7
Jhon Arias <7honarias@gmail.com>
2020-12-22 10:28:40 +0000

    [ADD] - function mod head of list

---
+++ op_mode.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+/**
+ * op_mod - function to mod top two elements of a stack
+ * @stack: double pointer of stack_t type to head of stack
+ * @line_number: unsigned int type
+ * Return: stack with new node and removed node
+ */
+void op_mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	int len = 0;
+
+	len = list_len(stack);
+	if (len < 2 || stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n",
+			line_number);
+		gvars.ret_val = -1;
+		return;
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n",
+			line_number);
+		gvars.ret_val = -1;
+		return;
+	}
+	else
+	{
+		temp->next->n %= (*stack)->n;
+		*stack = (*stack)->next;
+		free(temp);
+		(*stack)->prev = NULL;
+	}
+}
